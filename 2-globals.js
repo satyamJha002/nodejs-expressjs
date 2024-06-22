767e9597050f6dc3db3af038704187d708f9feeb
satyamJha002 <satyam073000@gmail.com>
2024-06-22 18:57:25 +0530

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,6 @@
+console.log(__dirname);
+
+let count = 0;
+setInterval(() => {
+  console.log(count++);
+}, 1000);
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,8 @@
+const items = ["item1", "item2"];
+
+const person = {
+  name: "bob",
+  age: 42,
+};
+
+module.exports = { items, person };
